f82dc7a3270c7a095f9e3ebb4d25adcc99f4e545
wl4dig3 <wladige@gmail.com>
2024-03-08 22:17:21 -0300

    se importa cdn bootstrap y placeholder a inputs

--- src/components/formulario/Formulario.jsx
+++ src/components/formulario/Formulario.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Formulario.css';
+import Button from 'react-bootstrap/Button';
 
 function Formulario() {
   return (
-    <section className='section-formulario'>
-      <input name="nombre" type="text" />
-      <input name="email" type="email" />
-      <input name="password" type="password" />
-      <input name="repeat-password" type="password" />
+    <section className="section-formulario">
+      <input placeholder='Nombre' name="nombre" type="text" />
+      <input placeholder='tu-email@ejemplo.com' name="email" type="email" />
+      <input placeholder='Contraseña' name="password" type="password" />
+      <input placeholder='Confirma tu contraseña' name="repeat-password" type="password" />
+      <Button variant="success" size="lg">
+        Registrarse
+      </Button>
     </section>
   );
 }
