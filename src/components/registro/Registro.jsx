312706880c439edcf7400b30f489c76cbc3f3998
wl4dig3 <wladige@gmail.com>
2024-03-10 17:18:53 -0300

    e agrega fontawesome

--- src/components/registro/Registro.jsx
+++ src/components/registro/Registro.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Registro.css';
 import Formulario from '../formulario/Formulario';
+import SocialButton from '../socialButton/SocialButton';
 
 function Registro() {
   return (
     <section className='section-registro'>
         <h1>Crea una cuenta</h1>
+        <SocialButton />
         <Formulario />
 
     </section>
--- src/components/socialButton/SocialButton.jsx
+++ src/components/socialButton/SocialButton.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faLinkedin, faGit } from '@fortawesome/free-brands-svg-icons'
+import './SocialButton.css';
+// import { Button } from 'react-bootstrap';
 
 function SocialButton() {
   return (
-    <div>SocialButton</div>
-  )
+    <>
+    <section className="container-social-buttons">
+      <div className="container-button">
+        <button className="button">
+          <FontAwesomeIcon icon={faFacebook} />
+        </button>
+      </div>
+      <div className="container-button">
+        <button className="button">
+          <FontAwesomeIcon icon={faGit} />
+        </button>
+      </div>
+      <div className="container-button">
+        <button className="button">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </button>
+      </div>
+    </section>
+      <p className='texto-social-buttons'>O usa tu email para registarte</p>
+    </>
+  );
 }
 
 export default SocialButton
