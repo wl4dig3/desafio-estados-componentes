296cd3979173a31f85960398a11814ca45027e29
wl4dig3 <wladige@gmail.com>
2024-03-10 17:39:09 -0300

    chore: se mapea y se envia desde props los iconos

--- src/components/registro/Registro.jsx
+++ src/components/registro/Registro.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import './Registro.css';
 import Formulario from '../formulario/Formulario';
 import SocialButton from '../socialButton/SocialButton';
+import { faFacebook, faLinkedin, faGit } from '@fortawesome/free-brands-svg-icons';
 
 function Registro() {
+  const iconos = [faFacebook, faLinkedin, faGit];
   return (
-    <section className='section-registro'>
-        <h1>Crea una cuenta</h1>
-        <SocialButton />
-        <Formulario />
-
+    <section className="section-registro">
+      <h1>Crea una cuenta</h1>
+      <div className='registro-container-iconos-sociales'>
+        {iconos.map((item, index) => {
+          return <SocialButton key={index} icon={item} />;
+        })}
+      </div>
+      <p className='texto-social-buttons'>O usa tu email para registarte</p>
+      {/* <SocialButton icon={iconos.map( (item)=> {item.icon})} /> */}
+      <Formulario />
     </section>
-  )
+  );
 }
 
 export default Registro;
--- src/components/socialButton/SocialButton.jsx
+++ src/components/socialButton/SocialButton.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faLinkedin, faGit } from '@fortawesome/free-brands-svg-icons'
 import './SocialButton.css';
-// import { Button } from 'react-bootstrap';
 
-function SocialButton() {
+// eslint-disable-next-line react/prop-types
+function SocialButton({icon}) {
   return (
     <>
     <section className="container-social-buttons">
       <div className="container-button">
         <button className="button">
-          <FontAwesomeIcon icon={faFacebook} />
-        </button>
-      </div>
-      <div className="container-button">
-        <button className="button">
-          <FontAwesomeIcon icon={faGit} />
-        </button>
-      </div>
-      <div className="container-button">
-        <button className="button">
-          <FontAwesomeIcon icon={faLinkedin} />
+          <FontAwesomeIcon icon={icon} />
         </button>
       </div>
     </section>
-      <p className='texto-social-buttons'>O usa tu email para registarte</p>
     </>
   );
 }
