ad78f06012d5e607aec8dad4b0b7d04d51ef1ec9
wl4dig3 <wladige@gmail.com>
2024-03-10 21:04:42 -0300

    cambiando texto del alert

--- src/components/alert/Alerta.jsx
+++ src/components/alert/Alerta.jsx
@@ -1,7 +1,7 @@
 import Alert from 'react-bootstrap/Alert';
 
 function Alerta() {
-  return <Alert variant={'warning'}>This is a alert—check it out!</Alert>;
+  return <Alert variant={'warning'}>Completa todos los campos!</Alert>;
 }
 
 export default Alerta;
